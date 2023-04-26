a1e9ca1d8c56adc09ba0843de51d7145cb30639a
Nerdytoo <39653510+olalemi@users.noreply.github.com>
2023-04-26 15:43:15 +0100

    security update

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,6 +1,6 @@
 const handleSignin = (db, bcrypt) => (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
+  const { email, name, password } = req.body;
+  if (!email || !name || !password) {
     return res.status(400).json('incorrect form submission');
   }
   db.select('email', 'hash').from('login')
